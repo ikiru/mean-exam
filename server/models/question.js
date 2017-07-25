4089224558d5e82e61979c87cd14731584e0bfc9
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-25 12:38:42 -0500

    models done

---
+++ server/models/question.js
@@ -0,0 +1,25 @@
+let mongoose = require("mongoose");
+
+// MOdel creation and validation
+let QuestionSchema = new mongoose.Schema(
+  {
+
+    question:
+      {
+        type: String,
+        required: [true, "Questiom cannot be blank"]
+          unique: [true, "Question is already used"]
+          minlength: [10, "Question should be at least 10 characters"]
+      },
+
+
+    answer: {
+        type: String,
+        required: [true, "Answer cannot be blank"]
+        minlength: [5, "Answer should be at least 5 characters"]
+      },
+
+  { timestamps: true }
+);
+
+mongoose.model("Question", QuestionSchema);
